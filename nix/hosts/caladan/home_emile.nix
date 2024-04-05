37a4d293a518829988517ea80b4466379bb2b753
Emile <git@emile.space>
2024-04-05 18:38:47 +0200

    emacs config
    Simplified it once again to apply the complex config later on again

--- nix/hosts/caladan/home_emile.nix
+++ nix/hosts/caladan/home_emile.nix
@@ -52,6 +52,14 @@
       '';
     };
 
+    emacs = {
+      enable = true;
+      package = pkgs.emacs;  # replace with pkgs.emacs-gtk, or a version provided by the community overlay if desired.
+      extraConfig = ''
+        (setq standard-indent 2)
+      '';
+    };
+
     kitty = {
       enable = true;
 
