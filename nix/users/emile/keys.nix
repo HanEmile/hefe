a48019448bbefde704a2e1dfbaf5161735b16c27
Emile <git@emile.space>
2024-06-09 00:29:24 +0200

    defininig a general purpose emile user with my ssh keys

---
+++ nix/users/emile/keys.nix
@@ -0,0 +1,9 @@
+{ ... }:
+
+{
+	all = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPZi43zHEsoWaQomLGaftPE5k0RqVrZyiTtGqZlpWsew emile@caladan"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzLZ56SEgwZZ0OusTdSDDhpMlxSg1zPNdRLuxKOfrR5 emile@chusuk"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMoHWyC9r0LVk6UlkhBWAJph0F6KHYHh83EI5U9wtfq2 shortcuts@ginaz"
+	];
+}
