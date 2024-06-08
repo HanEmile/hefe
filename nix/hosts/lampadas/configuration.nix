9c12c731cee80852f4ce42089b94140f0e35a60a
Emile <git@emile.space>
2024-06-09 00:19:52 +0200

    added the ios shortcuts ssh key to lampadas

--- nix/hosts/lampadas/configuration.nix
+++ nix/hosts/lampadas/configuration.nix
@@ -8,6 +8,7 @@ let
   emile_keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPZi43zHEsoWaQomLGaftPE5k0RqVrZyiTtGqZlpWsew emile@caladan"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzLZ56SEgwZZ0OusTdSDDhpMlxSg1zPNdRLuxKOfrR5 emile@chusuk"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMoHWyC9r0LVk6UlkhBWAJph0F6KHYHh83EI5U9wtfq2 shortcuts@ginaz"
   ];
 in {
   imports =
