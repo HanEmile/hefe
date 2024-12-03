30b941cbf0cbf34f6379e7d7330b08ee4ad78597
Emile <git@emile.space>
2024-12-03 21:49:48 +0100

    (pretalx_old) version bump via overlay

--- nix/pkgs/overlay.nix
+++ nix/pkgs/overlay.nix
@@ -2,4 +2,8 @@ final: prev: {
   vokobe = final.callPackage ./vokobe { inherit (final) naersk; };
   r2wars-web = final.callPackage ./r2wars-web { };
   remarvin = final.callPackage ./remarvin { };
+
+  pretalx_old = prev.pretalx.overrideAttrs ( old: {
+    version = "2024.1.0";
+  });
 }
