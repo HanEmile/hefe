4d08790c43b2d0720ef43b657a651a7c541d30d2
Emile <git@emile.space>
2025-02-12 21:27:16 +0100

    template: more formatting, this time the spaces in the default.nix

--- nix/templates/goapp/backend/default.nix
+++ nix/templates/goapp/backend/default.nix
@@ -1,21 +1,21 @@
 { pkgs, packagename, ... }:
 
 let
-	version = "0.0.1";
+  version = "0.0.1";
 in
 pkgs.buildGoModule {
-	name = "${packagename}-${version}";
-	pname = "${packagename}";
-	version = "${version}";
+  name = "${packagename}-${version}";
+  pname = "${packagename}";
+  version = "${version}";
 
-	src = ./.;
+  src = ./.;
   subPackages = [ "src" ];
-	vendorHash = "sha256-8wYERVt3PIsKkarkwPu8Zy/Sdx43P6g2lz2xRfvTZ2E=";
+  vendorHash = "sha256-8wYERVt3PIsKkarkwPu8Zy/Sdx43P6g2lz2xRfvTZ2E=";
 
-	postInstall = ''
-		mkdir -p $out
-		mv $out/bin/src $out/bin/${packagename}
-	'';
+  postInstall = ''
+    mkdir -p $out
+    mv $out/bin/src $out/bin/${packagename}
+  '';
 
-	doCheck = true;
+  doCheck = true;
 }
