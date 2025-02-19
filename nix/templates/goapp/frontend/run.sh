e220cd7ed1ff8b9a84e4660519ca0f74720f9e6e
Emile <git@emile.space>
2025-02-19 19:55:35 +0100

    test creds disclamer

--- nix/templates/goapp/frontend/run.sh
+++ nix/templates/goapp/frontend/run.sh
@@ -1,3 +1,4 @@
+# these are tests creds that aren't valid, try it :D
 export CLIENT_ID=goapp
 export CLIENT_SECRET=KGFO5LQnUxu1Zs.35gOem3MaG8odthg1U0v0.kScVPS6TPTWVRnAdT_nj4PYYSfuU6jdzTM6
 export CLIENT_CALLBACK_URL=http://localhost:8080/oauth2/callback
